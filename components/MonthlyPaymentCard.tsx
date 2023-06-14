0725f5c7576a060581fae7b31c1d48a7a91d68d8
Gabriel Barreto <gh.barreto@hotmail.com>
2023-06-13 18:47:38 -0700

    added box-shadow

--- components/MonthlyPaymentCard.tsx
+++ components/MonthlyPaymentCard.tsx
@@ -10,6 +10,8 @@ const StyledCard = styled.div`
     padding: 30px;
     border-radius: 25px;
     position: relative;
+
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
 `;
 
 const StyledText = styled(Text)`
